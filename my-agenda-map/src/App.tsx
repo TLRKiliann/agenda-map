f7b6b0b9d193551719b435acb3ed6dfb4cc3d4d4
TLRKiliann <philogenie@protonmail.com>
2022-10-01 20:30:05 +0200

    react-router-dom setup

--- my-agenda-map/src/App.tsx
+++ my-agenda-map/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { NavBar } from './Components/NavBar';
+import { MeetingPoint } from '';
+import { PhoneContact } from '';
+import { Map } from ''; 
 import './App.css';
 
 const App:React.FC = () => {
   return (
-    <div className="App">
-
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/MeetingPoint" element={<MeetingPoint />} />
+        <Route path="/PhoneContact" element={<PhoneContact />} />
+        <Route path="/Map" element={<Map />} />
+      </Routes>
     </div>
   );
 }

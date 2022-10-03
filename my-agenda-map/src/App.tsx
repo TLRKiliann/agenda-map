577350fc6cacb1b4bcf122ed2b4bb55a2949aac9
TLRKiliann <philogenie@protonmail.com>
2022-10-03 18:27:00 +0200

    error with routing corrected

--- my-agenda-map/src/App.tsx
+++ my-agenda-map/src/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { NavBar } from './Components/NavBar';
 import { MeetingPoint } from './Pages/MeetingPoint';
 import { PhoneContact } from './Pages/PhoneContact';
-import { MapMap } from './Pages/MapMap'; 
+import { Login } from './Pages/Login'; 
 import './App.scss';
 
 const App:React.FC = () => {
   return (
     <div>
-      <Router>
-        <NavBar />
-        <Route path="/" element={<MeetingPoint />} />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/PhoneContact" element={<PhoneContact />} />
-        <Route path="/MapMap" element={<MapMap />} />
-      </Router>
+        <Route path="/MeetingPoint" element={<MeetingPoint />} />
+      </Routes>
     </div>
   );
 }

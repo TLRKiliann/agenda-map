2e93b81a652a8a098077d51058974dc0bdbb59b8
TLRKiliann <philogenie@protonmail.com>
2022-10-05 01:40:23 +0200

    thank you TS

--- my-agenda-map/src/App.tsx
+++ my-agenda-map/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+//import { DataType } from './Models/model';
 import { Routes, Route } from 'react-router-dom';
 import { NavBar } from './Components/NavBar';
 import { MeetingPoint } from './Pages/MeetingPoint';
 import { PhoneContact } from './Pages/PhoneContact';
-import { Login } from './Pages/Login'; 
+import { Login } from './Pages/Login';
 import './App.scss';
 
 const App:React.FC = () => {

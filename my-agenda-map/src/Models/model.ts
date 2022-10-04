c6439f94765aa55cfd37cc06d74f6b357227cdfb
TLRKiliann <philogenie@protonmail.com>
2022-10-05 01:37:30 +0200

    model for type added

---
+++ my-agenda-map/src/Models/model.ts
@@ -0,0 +1,15 @@
+export type DataType = {
+  id: number
+  date: string
+  hour: string
+  location: string
+  firstname: string
+  lastname: string
+  phone: string
+  email: string
+  notice: string
+};
+
+export type DataTypeProps = {
+  datatype: DataType
+};

62221226870b9740a34f1000b4abe155eb404c2d
TLRKiliann <philogenie@protonmail.com>
2022-10-06 16:24:38 +0200

    delete with route URL ok

--- my-agenda-map/src/Services/meetingServices.tsx
+++ my-agenda-map/src/Services/meetingServices.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { DataType } from '../Models/model';
 
-export type DataTypeProps = {
+type DataTypeProps = {
   datatype: DataType[]
 }
 
@@ -9,8 +9,9 @@ const getUrl = 'http://localhost:3001/getAllMembers';
 //const getUrl = 'http://localhost:4002/api/getAllMembers';
 /*const postUrl = 'http://localhost:3001/api/create';
 const putUrl = 'http://localhost:3001/api/update';
-const deleteUrl = 'http://localhost:3001/api/delete';
-*/
+const deleteUrl = 'http://localhost:3001/api/delete';*/
+const deleteUrl = 'http://localhost:3001/getAllMembers';
+
 const getAll = () => {
   const request = axios.get<DataTypeProps>(getUrl)
   return request.then((response: any) => response.data)
@@ -31,15 +32,16 @@ const update = (id: number, newObject: any) => {
   const request = axios.put(`${putUrl}/${id}`, newObject)
   return request.then((response: any) => response.data)
 };
+*/
 
 const remove = (id: number) => {
   const request = axios.delete(`${deleteUrl}/${id}`)
   return request.then((response: any) => response.data)
 };
-*/
+
 
 const functionToCall = {
-  getAll
+  getAll, remove
 };
 
 export default functionToCall;

35967bdb4452537737fd0671aaf43d916b596bdb
TLRKiliann <philogenie@protonmail.com>
2022-10-12 15:40:53 +0200

    config for server improved

--- my-agenda-map/src/Services/meetingServices.tsx
+++ my-agenda-map/src/Services/meetingServices.tsx
@@ -6,42 +6,79 @@ type DataTypeProps = {
   datatype: DataType[]
 }
 
-const getUrl = '/getAllMembers';
+//console.log("app", app)
+
+const getUrl = '/api/getAllMembers';
 //const getUrl = 'http://localhost:3001/getAllMembers';
-const postUrl = '/create';
+const postUrl = '/api/create';
 //const postUrl = 'http://localhost:3001/getAllMembers';
-const putUrl = '/update';
+const putUrl = '/api/update/num';
+const putSaveUrl = '/api/update/pnum';
+const putNameUrl = '/api/update/name';
+const putSaveNameUrl = '/api/update/pname';
 //const putUrl = 'http://localhost:3001/getAllMembers';
-const deleteUrl = '/delete';
+const deleteUrl = '/api/delete';
 //const deleteUrl = 'http://localhost:3001/getAllMembers';
+//const URL_API = 'http://localhost:5000';
 
 const getAll = async () => {
   const request = app.get<DataTypeProps>(getUrl)
   return await request.then((response: any) => response.data)
 };
 
-const create = async (newObject: any) => {
-  const request = app.post(postUrl, newObject)
+const create = async (formData: any) => {
+  const request = app.post<any>(postUrl, formData)
   return await request.then((response: any) => response.data)
 };
 
-const updateNum = (id: number, newObject: any) => {
-  const request = app.put(`${putUrl}/${id}`, newObject)
-  return request.then((response: any) => response.data)
+
+const updateNum = async (id: number, formData: any) => {
+  //console.log("formData: ", formData)
+  const request = app.put<any>(`${putUrl}/${id}`, formData)
+  //console.log("[+] request", request)
+  return request.then<any>((response: any) => response.data)
 };
 
-/*const updateName = async (id: number, newObject: any) => {
-  const request = app.put(`${putUrl}/${id}`, newObject)
-  return await request.then((response: any) => response.data)
-};*/
 
-const remove = (id: number) => {
+const updatePostNum = async (id: number, formData: any) => {
+  const request = app.put<any>(`${putSaveUrl}/${id}`, formData)
+  return await request.then((response: any) => response.data)
+};
+
+const updateName = async (id: number, formData: any) => {
+  console.log(formData)
+  const request = app.put<any>(`${putNameUrl}/${id}`, formData)
+  console.log(request)
+  return await request.then((response: any) => response.data)
+};
+
+const updatePostName = async (id: number, formData: any) => {
+  const request = app.put<any>(`${putSaveNameUrl}/${id}`, formData)
+  return await request.then((response: any) => response.data)
+};
+
+const remove = async (id: number) => {
   const request = app.delete(`${deleteUrl}/${id}`)
-  return request.then((response: any) => response.data)
+  return await request.then((response: any) => response.data)
 };
 
 const functionToCall = {
-  getAll, create, updateNum, remove
+  getAll,
+  create,
+  updateNum,
+  updatePostNum,
+  updateName,
+  updatePostName,
+  remove
 };
 
 export default functionToCall;
+
+/*
+const updateNum = async (id: number, formData: any) => {
+  //console.log("formData: ", formData)
+  const request = app.put<any>(`${putUrl}/${id}`, formData)
+  //console.log("[+] request", request)
+  return request.then((response: any) => response.data)
+};
+*/
--- my-agenda-map/src/api/axiosconfig.ts
+++ my-agenda-map/src/api/axiosconfig.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export const app = axios.create({
-    baseURL: 'http://localhost:4002/api/',
-    withCredentials: true
-})
+    baseURL: 'http://localhost:5000',
+    headers: {
+        "Content-type": "application/json"
+    }
+});
